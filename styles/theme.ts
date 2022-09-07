bbe4c896232c08a0bf4ae9c2b27330de03fb0bbd
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-09-07 17:13:57 -0300

    Ajusta tema global

--- styles/theme.ts
+++ styles/theme.ts
@@ -5,4 +5,27 @@ export const theme = extendTheme({
     heading: 'Poppins, sans-serif',
     body: 'Poppins, sans-serif',
   },
+  colors: {
+    highlight: '#FFBA08',
+    'highlight50%': '#80FFBA08',
+    dark: {
+      headingsAndText: '#47585B',
+      info: '#999999',
+      'info50%': '#80999999',
+      black: '#000000',
+    },
+    light: {
+      headingsAndText: '#F5F8FA',
+      info: '#DADADA',
+      white: '#FFFFFF',
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'light.headingsAndText',
+        color: 'dark.headingsAndText',
+      },
+    },
+  },
 })
