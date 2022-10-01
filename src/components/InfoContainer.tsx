7423a471e72b24b9441fce6b46566f5658e68d92
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-10-01 20:29:40 -0300

    Cria componente de Info

---
+++ src/components/InfoContainer.tsx
@@ -0,0 +1,30 @@
+import { Box, HStack, Text, Tooltip } from '@chakra-ui/react'
+import { InfoOutlineIcon } from '@chakra-ui/icons'
+
+interface InfoProps {
+  description: string
+  count: number
+  hasToolTip?: boolean
+}
+
+export function Info({ count, description, hasToolTip = false }: InfoProps) {
+  return (
+    <HStack align="end">
+      <Box fontWeight="semibold" lineHeight={9} textAlign="center">
+        <Text fontSize="5xl" color="highlight">
+          {count}
+        </Text>
+
+        <Text fontSize="2xl">{description}</Text>
+      </Box>
+      {hasToolTip && (
+        <Tooltip
+          label="cidades do continente que estão entre as 100 mais visitadas do mundo"
+          color="light.headingsAndText"
+        >
+          <InfoOutlineIcon w={4} h={4} color="dark.info50%" />
+        </Tooltip>
+      )}
+    </HStack>
+  )
+}
